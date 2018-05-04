59517c683cc4970d9a9052fb0470e8679c8e1459
azash666 <ddelgadogomis@hotmail.com>
2018-05-04 11:17:49 +0200

    04-05-2018
    ESTÁ VIIIVOOOOOOO!!!!!

---
+++ Assets/Scripts/IAMovimiento.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class IAMovimiento : NetworkBehaviour {
+	public GameObject A;
+	public GameObject B;
+	public GameObject C;
+	private Rigidbody rb;
+	private detecta DA, DB, DC;
+	[SyncVar]
+	public Vector3 velocidad;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		DA = A.GetComponent<detecta> ();
+		DB = B.GetComponent<detecta> ();
+		DC = C.GetComponent<detecta> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		izda ();
+		dcha ();
+		avanza ();
+
+
+
+	}
+
+	void avanza(){
+		if (!DA.toca) {
+			if (rb.velocity.magnitude <= 10)
+				rb.AddForce (transform.forward * 200f);
+		} else {
+			//rb.AddForce (transform.forward * -200f);
+		}
+	}
+
+	void izda(){
+
+		if ((!DB.toca && DC.toca) || (DA.toca && DC.toca) || (DA.toca&&!DB.toca)) {
+			transform.Rotate (0, -50f*Time.deltaTime, 0);
+		}
+	}
+
+	void dcha(){
+		if (DB.toca && !DC.toca) {
+			transform.Rotate (0, 50f*Time.deltaTime, 0);
+		}
+	}
+}
---
+++ Assets/detecta.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detecta : MonoBehaviour {
+	public bool toca;
+	public int cosas;
+	public GameObject propietario;
+	// Use this for initialization
+	void Start(){
+		cosas = 0;
+	}
+	void Update () {
+		if (cosas == 0) {
+			toca = false;
+		} else {
+			toca = true;
+		}
+	}
+	void OnTriggerEnter(Collider colision){
+		GameObject hit = colision.gameObject;
+		if (hit != propietario && hit.tag!="Bala") {
+			cosas++;
+		}
+	}
+	void OnTriggerExit(Collider colision){
+		GameObject hit = colision.gameObject;
+		if (hit != propietario) {
+			cosas--;
+		}
+	}
+}
